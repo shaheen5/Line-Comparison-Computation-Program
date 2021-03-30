c585fc43e5410a2f3161c45e46f9892db612178b
unknown <miyashaheen1995@gmail.com>
2021-03-30 15:13:03 +0530

    Added: Welcome message

---
+++ LineComparisonComputation.java
@@ -0,0 +1,8 @@
+package LineComparisonProblem;
+
+public class LineComparisonComputation {
+	public static void main(String args[]) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+
+}
