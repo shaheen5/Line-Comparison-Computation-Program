41593205cba554f61d32b259a3f3a837bbc1d7cc
unknown <miyashaheen1995@gmail.com>
2021-03-30 15:55:09 +0530

    Added: Function to check equality of two lines

--- LineComparisonComputation.java
+++ LineComparisonComputation.java
@@ -6,8 +6,13 @@ public class LineComparisonComputation {
 	public static void main(String args[]) {
 		System.out.println("Welcome to Line Comparison Computation Program");
 		Scanner sc=new Scanner(System.in);
-		double length=CalculateLength(sc);
-		System.out.println("Length of Line is "+length);
+		System.out.println("Line 1 Co-ordinates:-");
+		double length1=CalculateLength(sc);
+		System.out.println("Length of Line 1 is "+length1);
+		System.out.println("Line 2 Co-ordinates:-");
+		double length2=CalculateLength(sc);
+		System.out.println("Length of Line 2 is "+length2);
+		checkEquality(length1,length2);
 	}
 	
 	static double CalculateLength(Scanner sc) {
@@ -24,5 +29,14 @@ public class LineComparisonComputation {
 		return Math.sqrt((x2_coordinate-x1_coordinate)*(x2_coordinate-x1_coordinate)+(y2_coordinate-y1_coordinate)*(y2_coordinate-y1_coordinate)); 		
 	}
 
+	static void checkEquality(double length1,double length2) {
+		String length_1=Double.toString(length1);
+		String length_2=Double.toString(length2);
+		if (length_1.equals(length_2))
+			System.out.println("Lines are Equal!");
+		else
+			System.out.println("Lines are not Equal!");
+		
+	}
 
 }
